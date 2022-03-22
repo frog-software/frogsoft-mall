f2d947bda8ea02d201dd88ddf0e5ff6ac67a06c6
sheeplin <1270610465@qq.com>
2022-03-22 17:17:53 +0800

    chore(frontend): initialize frontend project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello Frogsoft Mall" />
+</template>
+
+<script lang="ts">
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
