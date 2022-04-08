c0b618a719c458ed43fb26c5c3de493b8f6c1ce5
QQKdeGit <627948700@qq.com>
2022-04-08 12:18:49 +0800

    feat(frontend): change search location

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,78 +1,28 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
 
-const count              = ref<number>(0)
-const searchInputIsFocus = ref<boolean>(false)
-
-// 搜索栏可自动补全的热词
-interface hotSearchWordItem {
-  content: string,
-  link: string,
-}
-
-// 用户搜索的内容
-const searchContent  = ref<string>('')
-// 热词数组
-const hotSearchWords = ref<hotSearchWordItem[]>([])
-
-const queryHotWords = (currentSearchContent: string, callback: any) => {
-  const results = currentSearchContent
-      ? hotSearchWords.value.filter(createFilter(currentSearchContent))
-      : hotSearchWords.value
-
-  callback(results)
-}
-const createFilter  = (queryString: string) => {
-  return (hotSearchWord: hotSearchWordItem) => {
-    return (
-        hotSearchWord.content.toLowerCase().indexOf(queryString.toLowerCase()) === 0
-    )
-  }
-}
-
-const loadAllHotWords = () => {
-  return [
-    {value: 'AAAAAAA', link: ''},
-    {value: 'AAABBBB', link: ''},
-    {value: 'CCCDDDD', link: ''},
-  ]
-}
-
-onMounted(() => {
-  hotSearchWords.value = loadAllHotWords()
-})
-
+import Login from "./Login.vue";
 </script>
 
 <template>
-  <div style="padding-top: 24px; background-color: #222222">
+  <div style="padding: 24px 0 24px">
     <el-row
         align="middle"
-        justify="center"
         type="flex"
     >
-      <el-col :span="7" style="align-items: center; display: flex">
-        <el-button type="text" style="max-height: 1em; padding: 0; margin-top: -24px" >
-<!--          <img-->
-<!--              src="/FrogsoftMall-White.svg"-->
-<!--              alt="网站页眉LOGO"-->
-<!--              style="max-width: 25vw"-->
-<!--          >-->
-          <div class="LogoText">
-            <p class="LogoTextContent">Frogsoft Mall</p>
+      <el-col :span="1" :offset="4">
+        <img src="/public/frogsoft.svg" class="logoImage"/>
+      </el-col>
+
+      <el-col :span="4">
+        <el-button type="text">
+          <div class="logoText">
+            <p class="logoTextContent">Frogsoft Mall</p>
           </div>
         </el-button>
       </el-col>
-      <el-col :span="6" style="margin-bottom: 24px">
-        <el-autocomplete
-            v-model="searchContent"
-            :fetch-suggestions="queryHotWords"
-            placeholder="开始搜索"
-            style="width: 80%"
-            @focus="searchInputIsFocus=true"
-            @blur="searchInputIsFocus=false"
 
-        />
+      <el-col :span="1" :offset="8">
+        <Login />
       </el-col>
     </el-row>
   </div>
@@ -90,23 +40,29 @@ export default {
 </script>
 
 <style scoped>
-.LogoText {
-  /*background-color: #111;*/
-  justify-content: center;
-  align-items: center;
-
+.logoImage {
+  background: linear-gradient(to right, #FEAC5E, #C779D0, #4BC0C8);
+  width: 48px;
+  border-radius: 50%;
 }
 
-.LogoTextContent {
+.logoText {
+  justify-content: center;
+  align-items: center;
+}
+
+.logoTextContent {
   color: #555555;
   text-transform: uppercase;
   font-size: 52px;
   font-weight: bold;
   font-family: monospace;
   position: relative;
+  padding: 0;
+  margin: 0;
 }
 
-.LogoTextContent:after {
+.logoTextContent:after {
   content: "Frogsoft Mall";
   color: transparent;
   position: absolute;
@@ -116,10 +72,12 @@ export default {
   -webkit-background-clip: text;
 
   clip-path: circle(144px at 0% 50%);
-  animation: light 5s infinite;
+  animation: logoTextLight 5s infinite;
+  padding: 0;
+  margin: 0;
 }
 
-@keyframes light {
+@keyframes logoTextLight {
   0% {
     clip-path: circle(64px at 0% 50%);
   }
