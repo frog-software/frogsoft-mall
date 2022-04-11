a3afb0e61f30f8773456dadfaff75eca35fa90ee
sheeplin <1270610465@qq.com>
2022-04-11 17:17:28 +0800

    fix(frontend): rewrite vuex in TypeScript instead of JavaScript

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,14 +1,16 @@
-import { createStore } from 'vuex'
-// 创建一个新的 store 实例
-export const store = createStore({
-    state () {
-        return {
-            count: 0
-        }
-    },
-    mutations: {
-        increment (state) {
-            state.count++
-        }
+import {createStore, Store} from 'vuex'
+import {InjectionKey} from "vue";
+
+// 为 store state 声明类型
+export interface State {
+    count: number
+}
+
+// 定义 injection key
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+    state: {
+        count: 0
     }
 })
