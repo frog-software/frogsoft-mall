22d62c497a49b01b4bcdfa17d03a1236996919db
QQKdeGit <627948700@qq.com>
2022-04-21 04:05:25 +0800

    feat(frontend): add new navigator bar in header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
-import Login      from "./Login.vue";
-import RotateMenu from "./DrawerMenu.vue";
+import Login        from "./Login.vue";
+import RotateMenu   from "./DrawerMenu.vue";
+import NavigatorBar from "./NavigatorBar.vue";
+import test from "./test.vue";
 
 </script>
 
 <template>
-  <div style="padding-bottom: 8px; margin-top: 32px">
+  <div style="margin-top: 20px; user-select: none;">
     <el-row
         type="flex"
+        style="align-items: center"
     >
-      <el-col :span="1" :offset="3">
-        <img src="/frogsoft.svg" class="logoImage"/>
+<!--      <el-col :span="1" :offset="3">-->
+<!--        <img src="/frogsoft.svg" class="logoImage"/>-->
+<!--      </el-col>-->
+
+<!--      <el-col :span="4" :offset="3">-->
+<!--        <el-button type="text">-->
+<!--          <div class="logoText">-->
+<!--            <p class="logoTextContent">Frogsoft Mall</p>-->
+<!--          </div>-->
+<!--        </el-button>-->
+<!--        <router-link to="/" style="text-decoration: none; font-size: 32px; font-family: 'Arial Black'; color: white;">-->
+<!--          FrogSoft Mall ·-->
+<!--        </router-link>-->
+<!--      </el-col>-->
+
+      <el-col :span="12" :offset="3">
+        <NavigatorBar />
       </el-col>
 
-      <el-col :span="4">
-        <el-button type="text">
-          <div class="logoText">
-            <p class="logoTextContent">Frogsoft Mall</p>
-          </div>
-        </el-button>
-      </el-col>
-
-      <el-col :span="1" :offset="8">
+      <el-col :span="1" :offset="1">
         <Login />
       </el-col>
 
