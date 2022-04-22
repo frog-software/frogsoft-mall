247c70c42628cbe15c40f17d8ba560b5ab37ccbb
QQKdeGit <627948700@qq.com>
2022-04-23 01:02:35 +0800

    feat(frontend): delete navigator bar and search in Main.vue

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -48,25 +48,20 @@ onMounted(() => {
 <template>
 
 
-  <el-row justify="center" style="display: flex; align-items: center; margin-top: 32px">
-    <el-col :span="10">
-      <div>
-        <NavigatorBar/>
-      </div>
-    </el-col>
-    <el-col :span="4" :offset="1">
-      <el-autocomplete
-          v-model="searchContent"
-          :fetch-suggestions="queryHotWords"
-          placeholder="开始搜索"
-          @focus="searchInputIsFocus=true"
-          @blur="searchInputIsFocus=false"
-          size="large"
-          style="width: 100%; background-color: transparent"
-      >
-      </el-autocomplete>
-    </el-col>
-  </el-row>
+<!--  <el-row justify="center" style="display: flex; align-items: center; margin-top: 32px">-->
+<!--    <el-col :span="4" :offset="1">-->
+<!--      <el-autocomplete-->
+<!--          v-model="searchContent"-->
+<!--          :fetch-suggestions="queryHotWords"-->
+<!--          placeholder="开始搜索"-->
+<!--          @focus="searchInputIsFocus=true"-->
+<!--          @blur="searchInputIsFocus=false"-->
+<!--          size="large"-->
+<!--          style="width: 100%; background-color: transparent"-->
+<!--      >-->
+<!--      </el-autocomplete>-->
+<!--    </el-col>-->
+<!--  </el-row>-->
 
   <router-view/>
 
