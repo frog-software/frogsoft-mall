c186c8a090d44001ba784f1bebcbd72ce741c4b5
QQKdeGit <627948700@qq.com>
2022-05-08 15:37:40 +0800

    refactor(frontend): delete useless comment

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -35,21 +35,6 @@ const goSearchDetail = () => {
   <div style="margin-top: 20px; user-select: none;">
     <div id="header-root">
       <el-row type="flex" style="align-items: center">
-        <!--      <el-col :span="1" :offset="3">-->
-        <!--        <img src="/frogsoft.svg" class="logoImage"/>-->
-        <!--      </el-col>-->
-
-        <!--      <el-col :span="4" :offset="3">-->
-        <!--        <el-button type="text">-->
-        <!--          <div class="logoText">-->
-        <!--            <p class="logoTextContent">Frogsoft Mall</p>-->
-        <!--          </div>-->
-        <!--        </el-button>-->
-        <!--        <router-link to="/" style="text-decoration: none; font-size: 32px; font-family: 'Arial Black'; color: white;">-->
-        <!--          FrogSoft Mall ·-->
-        <!--        </router-link>-->
-        <!--      </el-col>-->
-
         <el-col :span="13" :offset="3">
           <NavigatorBar/>
         </el-col>
@@ -97,55 +82,6 @@ export default {
 </script>
 
 <style scoped>
-.logoImage {
-  background: linear-gradient(to right, #c1ab85, #f6eacc);
-  width: 48px;
-  border-radius: 50%;
-}
-
-.logoText {
-  justify-content: center;
-  align-items: center;
-  font-family: "Microsoft YaHei";
-}
-
-.logoTextContent {
-  color: #333333;
-  text-transform: uppercase;
-  font-size: 52px;
-  font-weight: bold;
-  position: relative;
-  padding: 0;
-  margin: 0;
-}
-
-.logoTextContent:after {
-  content: "Frogsoft Mall";
-  color: transparent;
-  position: absolute;
-  left: 0;
-  background: linear-gradient(to right, #c1ab85, #f6eacc);
-  background-clip: content-box;
-  -webkit-background-clip: text;
-
-  clip-path: circle(50px at 0% 50%);
-  animation: logoTextLight 5s infinite;
-  padding: 0;
-  margin: 0;
-}
-
-@keyframes logoTextLight {
-  0% {
-    clip-path: circle(64px at 0% 50%);
-  }
-  50% {
-    clip-path: circle(64px at 100% 50%);
-  }
-  100% {
-    clip-path: circle(64px at 0% 50%);
-  }
-}
-
 .button {
   height: 48px;
   width: 48px;
@@ -165,7 +101,6 @@ export default {
   display: flex;
   align-items: center;
   border-radius: 16px;
-  overflow: hidden;
   padding-left: 12px;
 }
 
