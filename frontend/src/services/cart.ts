cbe49e139557ba19d97008ba655b3a2cb3d3ce18
QQKdeGit <627948700@qq.com>
2022-05-12 21:50:54 +0800

    refactor(frontend): update cart page

--- frontend/src/services/cart.ts
+++ frontend/src/services/cart.ts
@@ -9,8 +9,25 @@
 //  History:    5月-03-2022   QQK  Created
 //
 //--------------------------------------------------------------------------
-import request              from "./request";
+import request                                    from "./request";
 import { CartResponseInfo } from "../types/cart";
-export const getCartDetails = async (user: string) => {
-  return await request.get<CartResponseInfo>(`/users/${user}/cart`)
+
+// CM03-01
+// export const editCartGoods = async (username: string, index: number, item: CartShopItemPostInfo) => {
+//   return await request.put<CartShopItemPostInfo>(`/customers/${username}/cart/${index.toFixed(0)}`, item)
+// }
+
+// CM03-03
+export const deleteCartGoods = async (username: string, index: number) => {
+  return await request.del(`/customers/${username}/cart/${index.toFixed(0)}`)
+}
+
+// cm03-04
+// export const addCartGoods = async (username: string, item: CartShopItemPostInfo) => {
+//   return await request.post<CartShopItemPostInfo>(`/customers/${username}/cart`, item)
+// }
+
+// CM03-05
+export const getCartDetails = async (username: string) => {
+  return await request.get<CartResponseInfo>(`/customers/${username}/cart`)
 }
--- frontend/src/views/Cart.vue
+++ frontend/src/views/Cart.vue
@@ -1,106 +1,141 @@
 <script setup lang="ts">
-import CheckBox                  from "../components/CheckBox.vue";
+import CheckBox                   from "../components/CheckBox.vue";
 import {
   GoodsFilled,
   Minus,
   Plus,
   Delete,
   More
-}                                from "@element-plus/icons-vue";
-import { onMounted, ref, watch } from "vue";
-import { CartResponseInfo }      from "../types/cart";
-import { getCartDetails }        from "../services/cart";
+}                                 from "@element-plus/icons-vue";
+import { ref }                    from "vue";
+import { CartResponseInfo }       from "../types/cart";
+import { getCartDetails }         from "../services/cart";
+import { ShopResponseInfo }         from "../types/shop";
+import { productDescriptionFormat } from "../utils/util";
+import { ProductSimpleInfo }        from "../types/product";
 
-const cartInfo     = ref<CartResponseInfo>()
-const selectedShop = ref<boolean[]>()
-const selectedItem = ref<boolean[][]>([])
-
-const sumPrice    = ref<number>(0)
-const selectedNum = ref<number>(0)
-const buyNum      = ref<number[][]>([])
-
-// 商品描述内容过长则需要省略
-const goodsDescriptionFormat = (str: string) => {
-  const MAX_LENGTH = 80;
-
-  if (str.length >= MAX_LENGTH)
-    return str.substring(0, MAX_LENGTH) + "..."
-  else return str
+interface CartShopItem {
+  shopInfo: ShopResponseInfo
+  productList: {
+    product: ProductSimpleInfo
+    addTime: string
+    amount: number
+    remarks: string
+    index: number
+    isSelected: boolean
+  }[]
+  isAllSelected: boolean
 }
 
-// 全选某个商店的商品
-const selectAllShopItems = (shopIndex: number) => {
-  selectedShop.value[shopIndex] = !selectedShop.value[shopIndex]
+const cartDetail   = ref<CartResponseInfo>([])  // 原数据
+const cartShopList = ref<CartShopItem[]>([])  // 用于页面展示
+const sumPrice     = ref<number>(0)
+const selectedNum  = ref<number>(0)
 
-  for (let i = 0; i < selectedItem.value[shopIndex].length; i++)
-    selectedItem.value[shopIndex][i] = selectedShop.value[shopIndex]
+
+// 全选某个商店的商品
+const selectAllShopItems = (shopName: string) => {
+  cartShopList.value.forEach(shop => {
+    if (shop.shopInfo.shopName === shopName) {
+      shop.isAllSelected = !shop.isAllSelected
+
+      shop.productList.forEach(item => {
+        item.isSelected = shop.isAllSelected
+        switchCardStyle(item.index, item.isSelected)
+      })
+    }
+  })
+  calcNumAndSumPrice()
+}
+
+// 选择某一款商品
+const selectOneShopItem = (shopName: string, index: number, isSelected: boolean) => {
+  switchCardStyle(index, isSelected)
+
+  cartShopList.value.forEach(shop => {
+    if (shop.shopInfo.shopName === shopName) {
+      if (isSelected) {
+        shop.isAllSelected = true
+        for (let i = 0; i < shop.productList.length; i++) {
+          if (!shop.productList[i].isSelected) {
+            shop.isAllSelected = false
+            break
+          }
+        }
+      } else {
+        shop.isAllSelected = false
+      }
+    }
+  })
+
+  calcNumAndSumPrice()
+}
+
+// 更改商品卡片的样式
+const switchCardStyle = (index: number, isSelected: boolean) => {
+  let elem  = document.getElementById('card-' + index)
+  let num   = document.getElementById('num-' + index)
+  let left  = document.getElementById('left-' + index)
+  let right = document.getElementById('right-' + index)
+
+  if (isSelected) {
+    elem.style.background   = 'rgba(246,234,204, 1)'
+    elem.style.boxShadow    = '0 0 30px 4px rgba(246,234,204,0.52)'
+    elem.style.color        = '#222222'
+    num.style.borderColor   = num.style.color = '#222222'
+    left.style.borderColor  = left.style.color = '#222222'
+    right.style.borderColor = right.style.color = '#222222'
+  } else {
+    elem.style.background   = 'transparent'
+    elem.style.boxShadow    = 'none'
+    elem.style.color        = '#ffffff'
+    num.style.borderColor   = num.style.color = '#ffffff'
+    left.style.borderColor  = left.style.color = '#ffffff'
+    right.style.borderColor = right.style.color = '#ffffff'
+  }
+}
+
+// 统计总数量和总价格
+const calcNumAndSumPrice = () => {
+  sumPrice.value    = 0
+  selectedNum.value = 0
+
+  cartShopList.value.forEach(shop => {
+    shop.productList.forEach(item => {
+      if (item.isSelected) {
+        sumPrice.value += item.amount * item.product.price
+        selectedNum.value++
+      }
+    })
+  })
 }
 
 getCartDetails('testUser').then(res => {
-  cartInfo.value = res
+  cartDetail.value = res
 
-  selectedShop.value = new Array(cartInfo.value?.shops.length).fill(false)
+  let tempItemList = res.cartItems.map((i, idx) => ({
+    ...i,
+    index: idx,
+    isSelected: false,
+  }))
 
-  for (let i = 0; i < cartInfo.value?.shops.length; i++) {
-    selectedItem.value?.push(new Array(cartInfo.value?.shops[i].items.length).fill(false))
-    buyNum.value?.push(new Array(cartInfo.value?.shops[i].items.length))
-    for (let j = 0; j < cartInfo.value?.shops[i].items.length; j++) {
-      buyNum.value[i][j] = cartInfo.value?.shops[i].items[j].number
-    }
-  }
-})
-
-onMounted(() => {
-  watch(selectedItem.value, (value) => {
-    sumPrice.value = selectedNum.value = 0
-
-    for (let i = 0; i < value.length; i++) {
-      let allSelected = true
-
-      for (let j = 0; j < value[i].length; j++) {
-        if (value[i][j] === false) allSelected = false
-
-        let elem  = document.getElementById('card' + i + '-' + j)
-        let num   = document.getElementById('num' + i + '-' + j)
-        let left  = document.getElementById('left' + i + '-' + j)
-        let right = document.getElementById('right' + i + '-' + j)
-
-        if (!elem || !num || !left || !right) break
-        if (value[i][j] === true) {
-          sumPrice.value += cartInfo.value?.shops[i].items[j].product.price * buyNum.value[i][j]
-          selectedNum.value++
-
-          elem.style.background   = 'rgba(246,234,204, 1)'
-          elem.style.boxShadow    = '0 0 30px 4px rgba(246,234,204,0.52)'
-          elem.style.color        = '#222222'
-          num.style.borderColor   = num.style.color = '#222222'
-          left.style.borderColor  = left.style.color = '#222222'
-          right.style.borderColor = right.style.color = '#222222'
-
-        } else {
-          elem.style.background   = 'transparent'
-          elem.style.boxShadow    = 'none'
-          elem.style.color        = '#ffffff'
-          num.style.borderColor   = num.style.color = '#ffffff'
-          left.style.borderColor  = left.style.color = '#ffffff'
-          right.style.borderColor = right.style.color = '#ffffff'
+  let shopNameList = []
+  tempItemList.forEach(item => {
+    if (shopNameList.findIndex(i => i === item.product.shop.shopName) === -1) {
+      cartShopList.value?.push({
+        shopInfo: item.product.shop,
+        productList: [item],
+        isAllSelected: false,
+      })
+      shopNameList.push(item.product.shop.shopName)
+    } else {
+      cartShopList.value?.forEach(shop => {
+        if (shop.shopInfo.shopName === item.product.shop.shopName) {
+          shop.productList.push(item)
         }
-      }
-
-      selectedShop.value[i] = allSelected;
+      })
     }
-  }, {deep: true})
-
-  watch(buyNum.value, (value) => {
-    sumPrice.value = 0
-    for (let i = 0; i < value.length; i++) {
-      for (let j = 0; j < value[i].length; j++) {
-        if (selectedItem.value[i][j]) {
-          sumPrice.value += cartInfo.value?.shops[i].items[j].product.price * buyNum.value[i][j]
-        }
-      }
-    }
-  }, {deep: true})
+  })
 })
 </script>
 
@@ -131,15 +166,15 @@ onMounted(() => {
         <el-divider style="width: 90%"/>
       </div>
 
-      <div v-if="cartInfo?.shops" v-for="(shop, shopIndex) in cartInfo?.shops">
+      <div v-if="cartDetail.itemCount" v-for="shop in cartShopList">
         <div style="display: flex; align-items: center; margin-left: 96px; color: #f6eacc">
-          <CheckBox @click="selectAllShopItems(shopIndex)" v-model:model-value="selectedShop[shopIndex]"/>
+          <CheckBox @click="selectAllShopItems(shop.shopInfo.shopName)" v-model:model-value="shop.isAllSelected"/>
           <el-icon :size="24" style="margin-left: 16px">
             <goods-filled/>
           </el-icon>
-          <p style="margin: 0 12px 0 4px">店铺：{{ shop.shop.shopName }}</p>
+          <p style="margin: 0 12px 0 4px">店铺：{{ shop.shopInfo.shopName }}</p>
           <el-rate
-              v-model="shop.shop.rate"
+              v-model="shop.shopInfo.rate"
               disabled
               :colors="['#f6eacc', '#f6eacc', '#f6eacc']"
               disabled-void-color="transparent"
@@ -147,11 +182,12 @@ onMounted(() => {
         </div>
 
         <div>
-          <div v-for="(item, itemIndex) in shop.items" style="width: 1140px; margin: 16px 0">
-            <div class="card-item" :id="'card' + shopIndex + '-' + itemIndex">
+          <div v-for="item in shop.productList" style="width: 1140px; margin: 16px 0">
+            <div class="card-item" :id="'card-' + item.index">
               <el-row style="width: 100%; align-items: center; ">
                 <el-col :span="1" style="justify-content: center">
-                  <CheckBox style="margin-left: 18px" v-model:model-value="selectedItem[shopIndex][itemIndex]"/>
+                  <CheckBox style="margin-left: 18px" v-model:model-value="item.isSelected"
+                            @click="selectOneShopItem(shop.shopInfo.shopName, item.index, item.isSelected)"/>
                 </el-col>
                 <el-col :span="3">
                   <el-image style="width: 100px; height: 100px" :src="item.product.thumb" fit="fill"/>
@@ -159,18 +195,18 @@ onMounted(() => {
                 <el-col :span="10" style="text-align: left">
                   <p style="margin: 0">{{ item.product.productName }}</p>
                   <p style="margin: 12px 0 0 0; color: #999999">{{
-                      goodsDescriptionFormat(item.product.description)
+                      productDescriptionFormat(item.product.description, 80)
                     }}</p>
                 </el-col>
                 <el-col :span="3">
                   <p>{{ item.product.price.toFixed(2) }}</p>
                 </el-col>
                 <el-col :span="4">
-                  <el-button :icon="Minus" class="num-control-button-left" :disabled="buyNum[shopIndex][itemIndex] === 1"
-                             @click="buyNum[shopIndex][itemIndex]--" :id="'left' + shopIndex + '-' + itemIndex"/>
-                  <input v-model="buyNum[shopIndex][itemIndex]" class="num-control" :id="'num' + shopIndex + '-' + itemIndex"/>
-                  <el-button :icon="Plus" class="num-control-button-right" @click="buyNum[shopIndex][itemIndex]++"
-                             :id="'right' + shopIndex + '-' + itemIndex"/>
+                  <el-button :icon="Minus" class="num-control-button-left" :disabled="item.amount === 1"
+                             @click="item.amount--" :id="'left-' + item.index"/>
+                  <input v-model="item.amount" class="num-control" :id="'num-' + item.index"/>
+                  <el-button :icon="Plus" class="num-control-button-right" @click="item.amount++"
+                             :id="'right-' + item.index"/>
                 </el-col>
                 <el-col :span="3">
                   <!-- TODO: 这里路由改一改，写成带有商品ID参数的那种 -->
@@ -178,7 +214,11 @@ onMounted(() => {
                     <el-button type="text" :icon="More" style="color: rgb(30, 144, 255)">详情</el-button>
                   </router-link>
                   <br/>
-                  <el-button type="text" style="color: rgb(248, 62, 91)" :icon="Delete">删除</el-button>
+                  <el-popconfirm title="Are you sure to delete this?">
+                    <template #reference>
+                      <el-button type="text" style="color: rgb(248, 62, 91)" :icon="Delete">删除</el-button>
+                    </template>
+                  </el-popconfirm>
                 </el-col>
               </el-row>
             </div>
@@ -196,7 +236,7 @@ onMounted(() => {
       <div style="display: flex; justify-content: center">
         <div style="width: 90%">
           <el-row style="justify-content: right">
-            <el-col :span="4" style="display: flex; align-items: center; height: 100%">
+            <el-col :span="10" style="display: flex; align-items: center; height: 100%; ">
               <div style="width: 100%; text-align: right; ">
                 <p style="margin: 0">总计：
                   <span style="color: #f6eacc; font-size: 24px; margin: 0">¥ {{ sumPrice.toFixed(2) }}</span>
