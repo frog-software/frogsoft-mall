480fb1a52f33ede8cb416015665644bbda809eaf
QQKdeGit <627948700@qq.com>
2022-05-17 02:21:04 +0800

    feat(frontend): update AboutPage.vue

--- frontend/src/views/AboutPage.vue
+++ frontend/src/views/AboutPage.vue
@@ -3,7 +3,13 @@ import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Frogsoft Mall" />
+<!--  <HelloWorld msg="Hello Frogsoft Mall" />-->
+  <div style="min-height: calc(100vh - 252px)">
+    <iframe id="inlineFrameExample"
+            title="Inline Frame Example"
+            src="https://github.com/frog-software/frogsoft-mall#readme">
+    </iframe>
+  </div>
 </template>
 
 <script lang="ts">
