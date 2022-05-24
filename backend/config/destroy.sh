88b68b8f56d89a5d01082a2ff6efbb49f109881b
Charlie Chiang <charlie_c_0129@outlook.com>
2022-05-24 14:20:27 +0800

    fix: ignore errors when stopping

--- backend/config/destroy.sh
+++ backend/config/destroy.sh
@@ -4,5 +4,5 @@
 SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
 cd "${SCRIPT_DIR}" || exit 1
 
-bash ./stop.sh || exit 1
-docker compose --file ../docker-compose.yml rm -sf  || exit 1
+bash ./stop.sh
+docker compose --file ../docker-compose.yml rm -sf
