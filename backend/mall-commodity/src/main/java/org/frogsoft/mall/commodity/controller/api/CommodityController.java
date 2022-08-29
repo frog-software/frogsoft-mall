44d07c1e8fa9a5bddc57c6523e293e975b1a3c9a
Nick-bit233 <58961585+Nick-bit233@users.noreply.github.com>
2022-08-29 10:34:33 +0800

    feat(backend)：modified PD01-05, add muti-SearchCriteria and Pageable response

--- backend/mall-commodity/src/main/java/org/frogsoft/mall/commodity/controller/api/CommodityController.java
+++ backend/mall-commodity/src/main/java/org/frogsoft/mall/commodity/controller/api/CommodityController.java
@@ -3,6 +3,8 @@ package org.frogsoft.mall.commodity.controller.api;
 import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
 import org.frogsoft.mall.commodity.controller.request.AddProductRequset;
+import org.frogsoft.mall.commodity.repository.specification.ProductSpecification;
+import org.frogsoft.mall.commodity.repository.specification.ProductSpecificationsBuilder;
 import org.frogsoft.mall.common.model.shop.Shop;
 import org.frogsoft.mall.common.model.user.UserDetail;
 import org.frogsoft.mall.common.request.ClientGetProductsRequest;
@@ -10,7 +12,11 @@ import org.frogsoft.mall.commodity.dto.ProductDto;
 import org.frogsoft.mall.common.model.product.Product;
 import org.frogsoft.mall.commodity.service.CommodityService;
 import org.frogsoft.mall.common.model.user.User;
+import org.frogsoft.mall.common.util.PageableResponseBodyWrapper;
 import org.frogsoft.mall.common.util.ResponseBodyWrapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -88,29 +94,42 @@ public class CommodityController {
     }
 
     // PD01-05：查找所有商品
-    // TODO：分页、复合查询和排序？
+    // 分页、复合查询和排序: 已完成
     @GetMapping("/all")
     public ResponseEntity<?> getAllProducts(
         @RequestParam(value = "shop_id",required = false) Long shop_id,
         @RequestParam(value = "keyword",required = false) String keyword,
-        @RequestParam(value = "orderby",required = false) String orderby
+        @RequestParam(value = "category",required = false) String category,
+        @RequestParam(value = "orderby",required = false) String orderby,
+        @RequestParam(value = "page",required = true) Integer page,
+        @RequestParam(value = "size",required = true) Integer size
     ) {
         ArrayList<ProductDto> res_body = null;
-
+        ProductSpecificationsBuilder builder = new ProductSpecificationsBuilder();
         if(shop_id != null){
-            res_body = commodityService.getAllProductsInShop(shop_id);
+            builder.with("shop","=", shop_id);
         }
-
         if(keyword != null){
-            res_body = commodityService.getAllProductsInNameKeyword(keyword);
+            builder.with("productName","=",keyword);
         }
-
+        if(category != null){
+            builder.with("category","=",category);
+        }
+        Pageable pageable;
+        if(orderby != null && !orderby.equals("")){
+            pageable = PageRequest.of(page, size, Sort.Direction.ASC, orderby);
+        }
+        else{
+            pageable = PageRequest.of(page, size);
+        }
+        res_body = commodityService.getAllProducts(builder.build(), pageable);
         if(res_body == null){
             return ResponseEntity.noContent().build();
         }
-
-        return new ResponseBodyWrapper<ArrayList<ProductDto>>()
+        return new PageableResponseBodyWrapper<ArrayList<ProductDto>>()
             .status(HttpStatus.OK)
+            .page(page)
+            .size(size)
             .body(res_body)
             .build();
     }
